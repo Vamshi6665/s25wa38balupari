099565cb5756d34ea25546a166a8b92849e063fd
Vamshi Krishna B <vamshi6665@gmail.com>
2025-03-25 13:42:56 -0500

    new

--- routes/computation.js
+++ routes/computation.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// Route to compute Math.exp()
 router.get('/', function (req, res, next) {
-    let x = req.query.x ? parseFloat(req.query.x) : (Math.random() * 10).toFixed(2); // Use query param or generate random x
-    let y = Math.exp(x).toFixed(2); // Apply Math.exp() and format to 2 decimal places
+    let x = req.query.x ? parseFloat(req.query.x) : (Math.random() * 10).toFixed(2); 
+    let y = Math.exp(x).toFixed(2); 
 
     res.send(`Math.exp applied to ${x} is ${y}`);
 });
